91b6ed4367a03ad97dd6ad899146d448b4536cb3
Rachel <rachelheidi@gmail.com>
2013-04-05 14:52:05 -0400

    Trying to extract opinion words

---
+++ project.py
@@ -0,0 +1,41 @@
+import nltk
+from nltk.corpus import movie_reviews, stopwords
+import string, random
+
+def opinion_keywords():
+    def determine_features(review, featurelist):
+        result = {}
+        for f in featurelist:
+            if f in review:
+                result[f] = 1
+            else:
+                result[f] = 0
+        return result
+
+    stopword_set = stopwords.words('english')
+    words =  movie_reviews.words()
+    fdist = nltk.FreqDist(word for word in movie_reviews.words()
+                    if word not in stopword_set and \
+                    word not in string.punctuation)
+    featurelist = fdist.keys()[:500]
+    
+    pos_reviews = [(movie_reviews.words(fileid), 'pos') for fileid in movie_reviews.fileids('pos')]
+    neg_reviews = [(movie_reviews.words(fileid), 'neg') for fileid in movie_reviews.fileids('neg')]
+    all_reviews = pos_reviews + neg_reviews
+    random.shuffle(all_reviews)
+
+    featuresets =[(determine_features(review, featurelist), label) 
+                    for review, label in all_reviews]
+
+    nreviews = len(all_reviews)
+    ntrain = int(nreviews * .75)
+    train, test = all_reviews[:ntrain], all_reviews[ntrain:]
+    classifier = nltk.NaiveBayesClassifier.train(train)
+    print "Accuracy of bayes classifier is: "
+    print nltk.classify.accuracy(classifier, test)
+    classifier.show_most_informative_features(10)
+    # ntest = nreviews
+
+    # classifier = nltk.NaiveBayesClassifier.train(train)
+
+opinion_keywords()
