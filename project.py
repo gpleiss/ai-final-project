292dc42a10d6bfda9ba2bd609099499c8eabd37c
Geoff Pleiss <gpleiss@gmail.com>
2013-04-08 23:18:29 -0400

    Using synsets of feature words to increase then number of features

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 import nltk
-from nltk.corpus import movie_reviews, stopwords
+from nltk.corpus import movie_reviews, stopwords, wordnet
 import string, random
 
 def opinion_keywords():
@@ -12,16 +12,23 @@ def opinion_keywords():
                 result[f] = 0
         return result
 
+    def words_in_synset(word):
+        possible_synsets = wordnet.synsets(word)
+        if len(possible_synsets) == 0:
+            return word.lower()
+        else:
+            return [syn.lower() for syn in possible_synsets[0].lemma_names]
+
     stopword_set = stopwords.words('english')
     words =  movie_reviews.words()
     fdist = nltk.FreqDist(word.lower() for word in movie_reviews.words()
                     if word.lower() not in stopword_set and \
                     word not in string.punctuation)
-    featurelist = [word for word in fdist.keys()[:3000]
-                    if nltk.pos_tag([word])[0][1] == 'JJ']
+    featurelist = fdist.keys()[:1000]
 
-    # print len(adjectives)
-    # featurelist = fdist.keys()[:1000]
+    featurelist_with_synset_words = []
+    for feature in featurelist:
+        featurelist_with_synset_words.extend(words_in_synset(feature))
     
     pos_reviews = [([word for word in movie_reviews.words(fileid)], 'pos') for fileid in movie_reviews.fileids('pos')]
     neg_reviews = [([word for word in movie_reviews.words(fileid)], 'neg') for fileid in movie_reviews.fileids('neg')]
@@ -38,9 +45,7 @@ def opinion_keywords():
     
     print "Accuracy of bayes classifier is: "
     print nltk.classify.accuracy(classifier, test)
-    classifier.show_most_informative_features(100)
-    # ntest = nreviews
+    classifier.show_most_informative_features(10)
 
-    # classifier = nltk.NaiveBayesClassifier.train(train)
 
 opinion_keywords()
